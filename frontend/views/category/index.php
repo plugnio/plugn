08720ff31988bc4509f69cf87e13dd4f3c8fae58
saoud77 <saoudalturki@gmail.com>
2020-02-16 16:44:51 +0300

    Redesign category page

--- frontend/views/category/index.php
+++ frontend/views/category/index.php
@@ -10,30 +10,37 @@ use yii\grid\GridView;
 $this->title = 'Manage Categories';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="category-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Category', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            [
-                'label' => 'Order',
-                'value' => 'sort_number'
-            ],
-            'category_name', 
-            'category_name_ar',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
 
 
-</div>
+    <div class="page-title"> <i class="icon-custom-left"></i>
+        <h3><?= Html::encode($this->title) ?></h3>
+        <p>
+            <?= Html::a('Create Category', ['create'], ['class' => 'btn btn-success']) ?>
+        </p>
+    </div>
+    <div class="row-fluid">
+        <div class="span12">
+            <div class="grid simple ">
+                <div class="grid-body ">
+
+                    <?php
+                    echo
+                    GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'filterModel' => $searchModel,
+                        'columns' => [
+                            [
+                                'label' => 'Order',
+                                'value' => 'sort_number'
+                            ],
+                            'category_name',
+                            'category_name_ar',
+                            ['class' => 'yii\grid\ActionColumn'],
+                        ],
+                        'options' => ['class' => 'table table-stripe'],
+                    ]);
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
