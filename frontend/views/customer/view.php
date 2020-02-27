9dd6fd5c7e1dec1d6b07d48478a65585a62aab5a
saoud77 <saoudalturki@gmail.com>
2020-02-27 19:08:09 +0300

    Add new customer table + re design order view pge:

--- frontend/views/customer/view.php
+++ frontend/views/customer/view.php
@@ -6,35 +6,41 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model common\models\Customer */
 
-$this->title = $model->customer_id;
+$this->title = $model->customer_name;
 $this->params['breadcrumbs'][] = ['label' => 'Customers', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="customer-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->customer_id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->customer_id], [
+        <?=
+        Html::a('Delete', ['delete', 'id' => $model->customer_id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
                 'method' => 'post',
             ],
-        ]) ?>
+        ])
+        ?>
     </p>
+    <div class="card">
+        <div class="card-body">
+            <?=
+            DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'customer_id',
+                    'customer_name',
+                    'customer_phone_number',
+                    'customer_email:email',
+                    'customer_created_at',
+                    'customer_updated_at',
+                ],
+                'options' => ['class' => 'table table-hover text-nowrap table-bordered'],
+            ])
+            ?>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'customer_id',
-            'customer_name',
-            'customer_phone_number',
-            'customer_email:email',
-            'customer_created_at',
-        ],
-    ]) ?>
-
-</div>
+        </div>
+    </div>
