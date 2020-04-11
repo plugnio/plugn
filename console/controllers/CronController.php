0bd1503d5975bd17105797a6785fa251252275c3
Saoud AlTurki <saoudalturki@gmail.com>
2020-04-11 19:46:12 +0300

    hotFix

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -11,9 +11,8 @@ class CronController extends \yii\console\Controller {
      * Used for testing only
      */
     public function actionIndex() {
-        $restaurant_model = \common\models\Restaurant::findOne('rest_dda9e02f-74c6-11ea-9c22-063f7dee6a14');
         $restaurant_theme = new \common\models\RestaurantTheme;
-        $restaurant_theme->restaurant_uuid = $restaurant_model->restaurant_uuid;
+        $restaurant_theme->restaurant_uuid = 'rest_dda9e02f-74c6-11ea-9c22-063f7dee6a14';
         $restaurant_theme->save();
     }
 
