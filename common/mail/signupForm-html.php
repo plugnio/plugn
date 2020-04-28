8bc17ed74725f4b06c7a4a47074da09289d7c668
Saoud AlTurki <saoudalturki@gmail.com>
2020-04-29 01:39:08 +0300

    update email template

--- common/mail/signupForm-html.php
+++ common/mail/signupForm-html.php
@@ -8,9 +8,9 @@ use yii\helpers\Html;
 <div class="verify-form">
     <p>Hello Big Boss,</p>
 
-    <h1>Company Name:</h1> <p> <?= $model->company_name ?></p>
-    <h1>Agent Name:</h1>  <p><?= $model->name ?></p>
-    <h1>Agent Phone:</h1> <p> <?= $model->phone ?></p>
-    <h1>Agent Email:</h1> <p> <?= $model->email ?></p>
-    
+    <h1 style="display: inline;">Company Name:</h1> <p> <?= $model->company_name ?></p>
+    <h1 style="display: inline;">Agent Name:</h1>  <p><?= $model->name ?></p>
+    <h1 style="display: inline;">Agent Phone:</h1> <p> <?= $model->phone ?></p>
+    <h1 style="display: inline;">Agent Email:</h1> <p> <?= $model->email ?></p>
+
 </div>
