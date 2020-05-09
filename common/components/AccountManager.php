0ceeb4ca3fb859b37ad41b14875274bd2c170870
saoud77 <saoudalturki@gmail.com>
2020-05-09 18:24:13 +0300

    hotFix

--- common/components/AccountManager.php
+++ common/components/AccountManager.php
@@ -33,9 +33,7 @@ class AccountManager  extends BaseObject
     {
         // This component must only be usable if agent is logged in
         if(Yii::$app->user->isGuest){
-                  return  Yii::$app->user->logout();
-
-//            throw new \yii\web\BadRequestHttpException('ILLEGAL USAGE OF ACCOUNT OWNERSHIP MANAGER');
+            throw new \yii\web\BadRequestHttpException('ILLEGAL USAGE OF ACCOUNT OWNERSHIP MANAGER');
         }
 
 //        // Getting a list of Restaurants this agent manages
