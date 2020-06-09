94a599df2b2d253eb17d544a35c77c5adbf46eb8
Saoud AlTurki <saoudalturki@gmail.com>
2020-06-09 22:29:01 +0300

    Update error message if there is no stock

--- common/models/OrderItem.php
+++ common/models/OrderItem.php
@@ -119,7 +119,7 @@ class OrderItem extends \yii\db\ActiveRecord {
 
         if ($insert) {
 
-            if ($this->qty == 1)
+            if ($this->qty  > $this->item->stock_qty)
                 return $this->addError('qty', $this->item->item_name . " is currently out of stock and unavailable.");
         }
         else {
