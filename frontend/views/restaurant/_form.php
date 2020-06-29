895a07981f8e2a560e726a9ad5f914d5caf4b54a
saoud77 <saoudalturki@gmail.com>
2020-06-30 01:00:59 +0300

    Ability to add google analytics id && facebook pixil id

--- frontend/views/restaurant/_form.php
+++ frontend/views/restaurant/_form.php
@@ -235,7 +235,10 @@ $this->registerJs($js);
     ?>
 
 
+    <?= $form->field($model, 'google_analytics_id')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'facebook_pixil_id')->textInput(['maxlength' => true]) ?>
+    
     <div class="form-group" style="background: #f4f6f9; padding-bottom: 10px; margin-bottom: 0px; padding-bottom: 15px; background:#f4f6f9 ">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success', 'style' => 'width: 100%;height: 50px;']) ?>
     </div>
