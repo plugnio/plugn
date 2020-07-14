ef4e958e997f3806bf94bcc0efb0739a2bfb0b06
Saoud AlTurki <saoudalturki@gmail.com>
2020-07-15 01:35:08 +0300

    hotFix

---
+++ frontend/assets/LoginAsset.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class LoginAsset extends AssetBundle {
+
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+
+  'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600',
+  'app-assets/vendors/css/vendors.min.css',
+  'app-assets/css/bootstrap.css',
+  'app-assets/css/bootstrap-extended.css',
+  'app-assets/css/colors.css',
+  'app-assets/css/components.css',
+  'app-assets/css/themes/dark-layout.css',
+  'app-assets/css/themes/semi-dark-layout.css',
+  'app-assets/css/core/menu/menu-types/vertical-menu.css',
+  'app-assets/css/core/colors/palette-gradient.css',
+  'app-assets/css/pages/authentication.css',
+  'assets/css/style.css',
+
+    ];
+    public $js = [
+      'app-assets/vendors/js/vendors.min.js',
+      'app-assets/js/core/app-menu.js',
+      'app-assets/js/core/app.js',
+      'app-assets/js/scripts/components.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+    ];
+
+}
