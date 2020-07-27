b8ea1bff88116dd4fa396bc161be984db2d3e833
saoud77 <saoudalturki@gmail.com>
2020-07-27 22:01:52 +0300

    Update out of stock order email template

--- common/mail/out-of-stock-order-html.php
+++ common/mail/out-of-stock-order-html.php
@@ -15,8 +15,5 @@ use common\models\Order;
       An issue occurred, in which you received a payment from one of your customers (<?=  $order->customer_name ?>) while the item was out of stock. Please check this issue with the customer to see if they require a refund.
     </p>
 
-    <div style="margin-left: auto; margin-right: auto; display:block;">
-<a href="<?= Yii::$app->request->baseUrl . '/order/view?id=' . $order->order_uuid . '&restaurantUuid=' . $order->restaurant_uuid ?>" style="display:inline-block;background:#ffffff;color:#000000;font-family:Proxima Nova, Arial, Arial, Helvetica, sans-serif;font-size:15px;font-weight:bold;line-height:120%;margin:0;text-decoration:none;text-transform:none;padding:10px 25px;mso-padding-alt:0px;border-radius:5px;" target="_blank">View order</a
-    </div>
 
 </div>
