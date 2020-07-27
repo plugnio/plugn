3ab1beee55434d99e93512aee7895d500d951df2
saoud77 <saoudalturki@gmail.com>
2020-07-27 23:30:20 +0300

    Fix duplicate email issue

--- common/models/Payment.php
+++ common/models/Payment.php
@@ -209,10 +209,9 @@ class Payment extends \yii\db\ActiveRecord {
     public function afterSave($insert, $changedAttributes) {
         parent::afterSave($insert, $changedAttributes);
 
-        if ($this->payment_current_status == 'CAPTURED'){
+        if ($this->payment_current_status == 'CAPTURED' && $this->received_callback){
           $this->order->changeOrderStatusToPending();
           $this->order->sendPaymentConfirmationEmail();
-
         }
     }
 
