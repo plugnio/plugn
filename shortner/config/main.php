b28f1664f1b371d59ea58f0b15a08750bb612d90
saoud77 <saoudalturki@gmail.com>
2020-10-14 14:44:49 +0300

    hotFix

--- shortner/config/main.php
+++ shortner/config/main.php
@@ -28,18 +28,18 @@ return [
             // this is the name of the session cookie used for login on the shortner
             'name' => 'advanced-shortner',
         ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
+        // 'log' => [
+        //     'traceLevel' => YII_DEBUG ? 3 : 0,
+        //     'targets' => [
+        //         [
+        //             'class' => 'yii\log\FileTarget',
+        //             'levels' => ['error', 'warning'],
+        //         ],
+        //     ],
+        // ],
+        // 'errorHandler' => [
+        //     'errorAction' => 'site/error',
+        // ],
         'urlManager' => [
                 'enablePrettyUrl' => true,
                 'showScriptName' => false,
