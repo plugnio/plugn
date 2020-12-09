463f161ad9eec65557074ce13833b93a1ee17221
saoud77 <saoudalturki@gmail.com>
2020-12-10 01:16:57 +0300

    hotFix

--- frontend/views/order/_form.php
+++ frontend/views/order/_form.php
@@ -121,6 +121,8 @@ $this->registerJs($js);
 
         <?= $form->field($model, 'customer_name')->textInput(['maxlength' => true]) ?>
 
+        <?= $form->field($model, 'customer_email')->textInput(['maxlength' => true, 'type' => 'email']) ?>
+
         <?= $form->field($model, 'customer_phone_number')->textInput(['maxlength' => true]) ?>
 
         <?= $form->field($model, 'special_directions')->textInput(['maxlength' => true]) ?>
