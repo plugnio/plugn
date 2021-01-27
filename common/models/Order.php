7e131ca13b8de1a71f33437f8926c82a536e5496
saoud77 <saoudalturki@gmail.com>
2021-01-27 19:55:37 +0300

    hotFix

--- common/models/Order.php
+++ common/models/Order.php
@@ -768,7 +768,7 @@ class Order extends \yii\db\ActiveRecord {
         }
 
 
-        if($this->restaurant->version == 1 $this->restaurant_branch_id && !$this->pickup_location_id ){
+        if($this->restaurant->version == 1 && $this->restaurant_branch_id && !$this->pickup_location_id ){
           $pickupLocation = BusinessLocation::find()
           ->where(['business_location_name' => $this->restaurantBranch->branch_name_en,
                    'business_location_name_ar' => $this->restaurantBranch->branch_name_ar
