cf0d1cce48327929f4532eca786f818c9c5895f8
saoud77 <saoudalturki@gmail.com>
2021-02-09 21:31:06 +0300

    hotIfx

--- common/models/Payment.php
+++ common/models/Payment.php
@@ -242,7 +242,7 @@ class Payment extends \yii\db\ActiveRecord {
     public function afterSave($insert, $changedAttributes) {
         parent::afterSave($insert, $changedAttributes);
 
-        if(isset($changedAttributes['payment_current_status']) && $changedAttributes['payment_current_status'] != 'CAPTURED' && $this->payment_current_status == 'CAPTURED') && $this->received_callback){
+        if( (isset($changedAttributes['payment_current_status']) && $changedAttributes['payment_current_status'] != 'CAPTURED' && $this->payment_current_status == 'CAPTURED') && $this->received_callback){
 
             $this->order->changeOrderStatusToPending();
             $this->order->sendPaymentConfirmationEmail();
