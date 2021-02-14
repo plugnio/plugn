252ee92d75de597d711e0ef5744e244334dff691
saoud77 <saoudalturki@gmail.com>
2021-02-14 17:05:45 +0300

    hotFix

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -234,7 +234,7 @@ class SiteController extends Controller {
                        0, //Comission
                       Url::to(['site/callback'], true),
                       $subscription_model->payment_method_id == 1 ? TapPayments::GATEWAY_KNET :  TapPayments::GATEWAY_VISA_MASTERCARD,
-                      $order->restaurant->warehouse_fee
+                      0 
               );
 
               $responseContent = json_decode($response->content);
