fa033d21d56d326010d087bb49917e56ef7c9921
saoud77 <saoudalturki@gmail.com>
2021-02-17 14:55:29 +0300

    Debug

--- common/models/Queue.php
+++ common/models/Queue.php
@@ -64,44 +64,54 @@ class Queue extends \yii\db\ActiveRecord {
 
         if ($this->queue_status == self::QUEUE_STATUS_PENDING) {
 
-            $store_model = $this->restaurant;
+          $store_model = $this->restaurant;
 
-            $getLastCommitResponse = Yii::$app->githubComponent->getLastCommit();
-
-            if ($getLastCommitResponse->isOk) {
-
-                $sha = $getLastCommitResponse->data['sha'];
-
-                //Replace test with store branch name
-                $branchName = 'refs/heads/' . $store_model->store_branch_name;
-                $createBranchResponse = Yii::$app->githubComponent->createBranch($sha, $branchName);
-
-                if ($createBranchResponse->isOk) {
+          $getLatestCommitResponse = Yii::$app->githubComponent->getLastCommit();
+          $sha = $getLatestCommitResponse->data['sha'];
+           $url = parse_url($store_model->restaurant_domain);
+          $branchName = 'refs/heads/' . $store_model->store_branch_name;
+          Yii::$app->githubComponent->createBranch($sha, $branchName);
+          Yii::$app->netlifyComponent->createSite($url['host'], $branchName);
 
 
-                   $url = parse_url($store_model->restaurant_domain);
-                    $createNewSiteResponse = Yii::$app->netlifyComponent->createSite($url['host'], $store_model->store_branch_name);
-
-                        if ($createNewSiteResponse->isOk) {
-
-                            $site_id = $createNewSiteResponse->data['site_id'];
-                            $store_model->site_id = $site_id;
-                            $store_model->save(false);
-
-                        } else {
-                            Yii::error('[Netlify > While Creating new site]' . json_encode($createNewSiteResponse->data), __METHOD__);
-                            return false;
-                        }
-
-                } else{
-                  Yii::error('[Github > Create branch]' . json_encode($createBranchResponse->data['message']) . ' RestaurantUuid: '. $store_model->restaurant_uuid, __METHOD__);
-                  return false;
-                }
-
-            } else {
-                Yii::error('[Github > Last commit]' . json_encode($getLastCommitResponse->data['message']) . ' RestaurantUuid: '. $store_model->restaurant_uuid, __METHOD__);
-                return false;
-            }
+            // $store_model = $this->restaurant;
+            //
+            // $getLatestCommitResponse = Yii::$app->githubComponent->getLastCommit();
+            //
+            // if ($getLatestCommitResponse->isOk) {
+            //
+            //     $sha = $getLatestCommitResponse->data['sha'];
+            //
+            //     //Replace test with store branch name
+            //     $branchName = 'refs/heads/' . $store_model->store_branch_name;
+            //     $createBranchResponse = Yii::$app->githubComponent->createBranch($sha, $branchName);
+            //
+            //     if ($createBranchResponse->isOk) {
+            //
+            //
+            //        $url = parse_url($store_model->restaurant_domain);
+            //         $createNewSiteResponse = Yii::$app->netlifyComponent->createSite($url['host'], $store_model->store_branch_name);
+            //
+            //             if ($createNewSiteResponse->isOk) {
+            //
+            //                 $site_id = $createNewSiteResponse->data['site_id'];
+            //                 $store_model->site_id = $site_id;
+            //                 $store_model->save(false);
+            //
+            //             } else {
+            //                 Yii::error('[Netlify > While Creating new site]' . json_encode($createNewSiteResponse->data), __METHOD__);
+            //                 return false;
+            //             }
+            //
+            //     } else{
+            //       Yii::error('[Github > Create branch]' . json_encode($createBranchResponse->data['message']) . ' RestaurantUuid: '. $store_model->restaurant_uuid, __METHOD__);
+            //       return false;
+            //     }
+            //
+            // } else {
+            //     Yii::error('[Github > Last commit]' . json_encode($getLatestCommitResponse->data['message']) . ' RestaurantUuid: '. $store_model->restaurant_uuid, __METHOD__);
+            //     return false;
+            // }
 
             $this->queue_status = Queue::QUEUE_STATUS_COMPLETE;
 
