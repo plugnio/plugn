0b725e0bfe86819e7584fb641a5c590413499a8e
saoud77 <saoudalturki@gmail.com>
2021-02-17 15:10:33 +0300

    Debug 4

--- common/models/Queue.php
+++ common/models/Queue.php
@@ -72,40 +72,44 @@ class Queue extends \yii\db\ActiveRecord {
 
             if ($getLatestCommitResponse->isOk) {
 
+                 $sha = $getLatestCommitResponse->data['sha'];
+                $url = parse_url($store_model->restaurant_domain);
+                 Yii::$app->githubComponent->createBranch($sha, $branchName);
+                 Yii::$app->netlifyComponent->createSite($url['host'], $store_model->store_branch_name);
 
-                $sha = $getLatestCommitResponse->data['sha'];
-
-                //Replace test with store branch name
-                $branchName = 'refs/heads/' . $store_model->store_branch_name;
-                $createBranchResponse = Yii::$app->githubComponent->createBranch($sha, $branchName);
-
-                if ($createBranchResponse->isOk) {
-
-                   $url = parse_url($store_model->restaurant_domain);
-
-                  Yii::$app->netlifyComponent->createSite($url['host'], $store_model->store_branch_name);
-
-
+                // $sha = $getLatestCommitResponse->data['sha'];
+                // 
+                // //Replace test with store branch name
+                // $branchName = 'refs/heads/' . $store_model->store_branch_name;
+                // $createBranchResponse = Yii::$app->githubComponent->createBranch($sha, $branchName);
                 //
+                // if ($createBranchResponse->isOk) {
                 //
                 //    $url = parse_url($store_model->restaurant_domain);
-                //     $createNewSiteResponse = Yii::$app->netlifyComponent->createSite($url['host'], $store_model->store_branch_name);
                 //
-                //         if ($createNewSiteResponse->isOk) {
+                //   Yii::$app->netlifyComponent->createSite($url['host'], $store_model->store_branch_name);
                 //
-                //             $site_id = $createNewSiteResponse->data['site_id'];
-                //             $store_model->site_id = $site_id;
-                //             $store_model->save(false);
                 //
-                //         } else {
-                //             Yii::error('[Netlify > While Creating new site]' . json_encode($createNewSiteResponse->data), __METHOD__);
-                //             return false;
-                //         }
-                //
-                // } else{
-                //   Yii::error('[Github > Create branch]' . json_encode($createBranchResponse->data['message']) . ' RestaurantUuid: '. $store_model->restaurant_uuid, __METHOD__);
-                //   return false;
-                }
+                // //
+                // //
+                // //    $url = parse_url($store_model->restaurant_domain);
+                // //     $createNewSiteResponse = Yii::$app->netlifyComponent->createSite($url['host'], $store_model->store_branch_name);
+                // //
+                // //         if ($createNewSiteResponse->isOk) {
+                // //
+                // //             $site_id = $createNewSiteResponse->data['site_id'];
+                // //             $store_model->site_id = $site_id;
+                // //             $store_model->save(false);
+                // //
+                // //         } else {
+                // //             Yii::error('[Netlify > While Creating new site]' . json_encode($createNewSiteResponse->data), __METHOD__);
+                // //             return false;
+                // //         }
+                // //
+                // // } else{
+                // //   Yii::error('[Github > Create branch]' . json_encode($createBranchResponse->data['message']) . ' RestaurantUuid: '. $store_model->restaurant_uuid, __METHOD__);
+                // //   return false;
+                // }
 
             } else {
                 Yii::error('[Github > Last commit]' . json_encode($getLatestCommitResponse->data['message']) . ' RestaurantUuid: '. $store_model->restaurant_uuid, __METHOD__);
