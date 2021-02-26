aaa2a47285b488dd3e308801cb6989222185af2e
saoud77 <saoudalturki@gmail.com>
2021-02-26 15:46:13 +0300

    try to fix duplicate customer iisue2

--- common/models/Order.php
+++ common/models/Order.php
@@ -929,6 +929,7 @@ class Order extends \yii\db\ActiveRecord {
               \Yii::error('[Duplicate customer22]' . json_encode(($customer_model)), __METHOD__); // Log error faced by user
               \Yii::error('[Duplicate customer3]' . json_encode((Restaurant::findOne($this->restaurant_uuid))), __METHOD__); // Log error faced by user
               \Yii::error('[Duplicate customer4]' . json_encode(($this)), __METHOD__); // Log error faced by user
+              \Yii::error('[Duplicate customer5]' . json_encode((Restaurant::findOne($this->restaurant_uuid))->restaurant_uuid)), __METHOD__); // Log error faced by user
 
                 $customer_model = new Customer();
                 $customer_model->restaurant_uuid = $this->restaurant_uuid;
