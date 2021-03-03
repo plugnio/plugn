a4ddccc8acf24b8cc922c85a0be30f6912ae6314
saoud77 <saoudalturki@gmail.com>
2021-03-03 18:48:43 +0300

    hotFix

--- api/modules/v2/controllers/ZapierController.php
+++ api/modules/v2/controllers/ZapierController.php
@@ -122,10 +122,16 @@ class ZapierController extends Controller {
 
 
                 $orders[$key]['id'] = $order['order_uuid'];
+                
                 $orders[$key]['estimated_time_of_arrival'] = date('c', strtotime($order['estimated_time_of_arrival']));
                 $orders[$key]['order_created_at'] = date('c', strtotime($order['order_created_at']));
                 $orders[$key]['order_updated_at'] =  date('c', strtotime($order['order_updated_at']));
-                $orders[$key]['scheduled_time_to'] =  date('c', strtotime($order['order_updated_at']));
+
+                $orders[$key]['scheduled_time_to'] =  date('c', strtotime($order['scheduled_time_to']));
+                $orders[$key]['scheduled_time_start_from'] =  date('c', strtotime($order['scheduled_time_start_from']));
+
+
+
                 $orders[$key]['customer_phone_number'] =   $customer_phone_number ;
 
 
