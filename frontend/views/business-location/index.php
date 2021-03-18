ac0ec06de1ec1041935d5cfb16e0af13ae0ef16e
saoud77 <saoudalturki@gmail.com>
2021-03-18 12:21:35 +0300

    hotFix

--- frontend/views/business-location/index.php
+++ frontend/views/business-location/index.php
@@ -84,17 +84,17 @@ $this->registerJs($js);
         </div>
 
 
-        <?php
-          echo Html::a('Delete',
-          ['delete', 'id' => $businessLocation->business_location_id, 'storeUuid' => $store->restaurant_uuid],
-          [
-            'style' => ' position: absolute; top: 10px; right: 10px; color:#EA5455',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this location?',
-                'method' => 'post',
-            ]
-          ],
-          )
+        <?
+          // echo Html::a('Delete',
+          // ['delete', 'id' => $businessLocation->business_location_id, 'storeUuid' => $store->restaurant_uuid],
+          // [
+          //   'style' => ' position: absolute; top: 10px; right: 10px; color:#EA5455',
+          //   'data' => [
+          //       'confirm' => 'Are you sure you want to delete this location?',
+          //       'method' => 'post',
+          //   ]
+          // ],
+          // )
         ?>
 
 
