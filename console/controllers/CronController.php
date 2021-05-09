3eff9360ddba9ec64fdb1918fa66262777fa569f
saoud77 <saoudalturki@gmail.com>
2021-05-09 03:59:39 +0300

    hotFix

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -457,6 +457,7 @@ class CronController extends \yii\console\Controller {
         $payments = Payment::find()
                 ->joinWith('order')
                 ->where(['!=', 'payment.payment_current_status', 'CAPTURED'])
+                ->orWhere(['!=', 'payment.payment_current_status', 'Paid'])
                 ->andWhere(['order.order_status' => Order::STATUS_ABANDONED_CHECKOUT])
                 ->andWhere(['order.items_has_been_restocked' => 0]) // if items hasnt been restocked
                 ->andWhere(['<', 'payment.payment_created_at', new Expression('DATE_SUB(NOW(), INTERVAL 15 MINUTE)')]);
