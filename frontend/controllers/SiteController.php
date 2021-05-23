c6ce762cf1fe3a6f5b2e3cd9677cd17c5af0aca4
saoud77 <saoudalturki@gmail.com>
2021-05-23 12:43:47 +0300

    hotFix

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -1224,7 +1224,7 @@ class SiteController extends Controller {
         $this->layout = 'login';
 
         $store_model = new Restaurant();
-        $store_model->version = 2;
+        $store_model->version = 3;
         $store_model->setScenario(Restaurant::SCENARIO_CREATE_STORE_BY_AGENT);
 
         $agent_model = new Agent();
