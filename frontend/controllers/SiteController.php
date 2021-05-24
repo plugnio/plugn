6776415a86e8de82754f820bf3cef6edb9145281
saoud77 <saoudalturki@gmail.com>
2021-05-24 14:44:34 +0300

    hotFix

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -19,6 +19,7 @@ use common\models\BusinessLocation;
 use common\models\Restaurant;
 use common\models\OrderItem;
 use common\models\Category;
+use common\models\RestaurantPaymentMethod;
 use common\models\Order;
 use common\models\Plan;
 use common\models\PaymentMethod;
