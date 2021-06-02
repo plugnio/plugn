61039d35162531d2b241ae226eeab9953e365345
Krushn Kathrecha <kathrechakrushn@gmail.com>
2021-06-02 13:14:57 +0530

    krushn env

---
+++ vendors/config/main-local.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'Xmdf2ohM-r4uITeOj5OqJ_Zmwjr2Qnm-',
+        ],
+    ],
+];
+
+return $config;
