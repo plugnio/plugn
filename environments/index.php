25c7c49a71d8dadf1d0b724b78e6210524f785d2
saoud77 <saoudalturki@gmail.com>
2021-06-08 00:42:56 +0300

    Add staging env

--- environments/index.php
+++ environments/index.php
@@ -89,6 +89,36 @@ return [
             'shortner/config/main-local.php',
         ],
     ],
+    'Staging-Server' => [
+        'path' => 'staging',
+        'setWritable' => [
+            'backend/runtime',
+            'backend/web/assets',
+            'backend/web/uploads/project-files',
+            'console/runtime',
+            'frontend/runtime',
+            'frontend/web/assets',
+            'frontend/web/uploads/',
+            'api/runtime',
+            'api/web/assets',
+            'agent/runtime',
+            'agent/web/assets',
+            'shortner/runtime'
+        ],
+        'setExecutable' => [
+            'yii',
+            'yii_test',
+            'fixture-generate-and-load'
+        ],
+        'setCookieValidationKey' => [
+            'backend/config/main-local.php',
+            'common/config/codeception-local.php',
+            'frontend/config/main-local.php',
+            'api/config/main-local.php',
+            'agent/config/main-local.php',
+            'shortner/config/main-local.php',
+        ],
+    ],
     'Production' => [
         'path' => 'prod',
         'setWritable' => [
---
+++ environments/staging/common/config/params-local.php
@@ -0,0 +1,5 @@
+<?php
+return [
+  'apiEndpoint' => 'https://api.dev.plugn.io',
+  'frontendUrl' => 'http://dashboard.dev.plugn.io',
+];
