4f4d58a2834c2f81917d88434a41524614fccc92
Krushn Kathrecha <kathrechakrushn@gmail.com>
2021-06-08 19:35:59 +0530

    ability to update/add/delete item

--- agent/modules/v1/controllers/BusinessLocationController.php
+++ agent/modules/v1/controllers/BusinessLocationController.php
@@ -9,17 +9,19 @@ use yii\helpers\Url;
 use yii\web\NotFoundHttpException;
 use common\models\BusinessLocation;
 
-class BusinessLocationController extends Controller {
+class BusinessLocationController extends Controller
+{
 
-    public function behaviors() {
-        $behaviors = parent::behaviors();
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors ();
 
         // remove authentication filter for cors to work
         unset($behaviors['authenticator']);
 
         // Allow XHR Requests from our different subdomains and dev machines
         $behaviors['corsFilter'] = [
-            'class' => \yii\filters\Cors::className(),
+            'class' => \yii\filters\Cors::className (),
             'cors' => [
                 'Origin' => Yii::$app->params['allowedOrigins'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
@@ -36,20 +38,21 @@ class BusinessLocationController extends Controller {
         ];
 
         // Bearer Auth checks for Authorize: Bearer <Token> header to login the user
-              $behaviors['authenticator'] = [
-                  'class' => \yii\filters\auth\HttpBearerAuth::className(),
-              ];
-              // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
-              $behaviors['authenticator']['except'] = ['options'];
+        $behaviors['authenticator'] = [
+            'class' => \yii\filters\auth\HttpBearerAuth::className (),
+        ];
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options'];
 
-              return $behaviors;
+        return $behaviors;
     }
 
     /**
      * @inheritdoc
      */
-    public function actions() {
-        $actions = parent::actions();
+    public function actions()
+    {
+        $actions = parent::actions ();
         $actions['options'] = [
             'class' => 'yii\rest\OptionsAction',
             // optional:
@@ -61,59 +64,57 @@ class BusinessLocationController extends Controller {
 
 
     /**
-    * Get all store's branches
+     * Get all store's branches
      * @param type $id
      * @param type $store_uuid
      * @return type
      */
-    public function actionList($store_uuid) {
+    public function actionList($store_uuid)
+    {
 
-      $keyword = Yii::$app->request->get('keyword');
+        $keyword = Yii::$app->request->get ('keyword');
 
-      Yii::$app->accountManager->getManagedAccount($store_uuid);
+        Yii::$app->accountManager->getManagedAccount ($store_uuid);
 
-      $query =  BusinessLocation::find()->joinWith('country');
+        $query = BusinessLocation::find ()->joinWith ('country');
 
-      if ($keyword){
-        $query->where(['like', 'business_location_name', $keyword]);
-        $query->orWhere(['like', 'business_location_name_ar', $keyword]);
-        $query->orWhere(['like', 'country.country_name', $keyword]);
-        $query->orWhere(['like', 'country.country_name_ar', $keyword]);
-      }
+        if ($keyword) {
+            $query->where (['like', 'business_location_name', $keyword]);
+            $query->orWhere (['like', 'business_location_name_ar', $keyword]);
+            $query->orWhere (['like', 'country.country_name', $keyword]);
+            $query->orWhere (['like', 'country.country_name_ar', $keyword]);
+        }
 
-      $query->andWhere(['restaurant_uuid' => $store_uuid]);
+        $query->andWhere (['restaurant_uuid' => $store_uuid]);
 
-      return new ActiveDataProvider([
-        'query' => $query
-      ]);
+        return new ActiveDataProvider([
+            'query' => $query
+        ]);
 
-  }
+    }
 
     /**
      * Create Business Location
      * @return array
      */
-    public function actionCreate() {
-
-        $store_uuid = Yii::$app->request->getBodyParam("store_uuid");
-        Yii::$app->accountManager->getManagedAccount($store_uuid);
-
+    public function actionCreate()
+    {
+        $store = Yii::$app->accountManager->getManagedAccount ();
 
         $model = new BusinessLocation();
-        $model->restaurant_uuid = $store_uuid;
-        $model->country_id =  Yii::$app->request->getBodyParam("country_id");
-        $model->business_location_name =  Yii::$app->request->getBodyParam("business_location_name");
-        $model->business_location_name_ar =  Yii::$app->request->getBodyParam("business_location_name_ar");
-        $model->support_pick_up = (int) Yii::$app->request->getBodyParam("support_pick_up");
-        $model->business_location_tax = (double) Yii::$app->request->getBodyParam("business_location_tax");
-        $model->mashkor_branch_id =  Yii::$app->request->getBodyParam("mashkor_branch_id");
-        $model->armada_api_key =  Yii::$app->request->getBodyParam("armada_api_key");
-        $model->address =  Yii::$app->request->getBodyParam("address");
-        $model->latitude =  Yii::$app->request->getBodyParam("latitude");
-        $model->longitude =  Yii::$app->request->getBodyParam("longitude");
+        $model->restaurant_uuid = $store->restaurant_uuid;
+        $model->country_id = Yii::$app->request->getBodyParam ("country_id");
+        $model->business_location_name = Yii::$app->request->getBodyParam ("business_location_name");
+        $model->business_location_name_ar = Yii::$app->request->getBodyParam ("business_location_name_ar");
+        $model->support_pick_up = (int)Yii::$app->request->getBodyParam ("support_pick_up");
+        $model->business_location_tax = (double)Yii::$app->request->getBodyParam ("business_location_tax");
+        $model->mashkor_branch_id = Yii::$app->request->getBodyParam ("mashkor_branch_id");
+        $model->armada_api_key = Yii::$app->request->getBodyParam ("armada_api_key");
+        $model->address = Yii::$app->request->getBodyParam ("address");
+        $model->latitude = Yii::$app->request->getBodyParam ("latitude");
+        $model->longitude = Yii::$app->request->getBodyParam ("longitude");
 
-
-        if (!$model->save()) {
+        if (!$model->save ()) {
             return [
                 "operation" => "error",
                 "message" => $model->errors
@@ -123,116 +124,103 @@ class BusinessLocationController extends Controller {
         return [
             "operation" => "success",
             "message" => "Business Location created successfully",
-            "model" => BusinessLocation::findOne($model->business_location_id)
+            "model" => BusinessLocation::findOne ($model->business_location_id)
         ];
-
     }
 
+    /**
+     * Update Business Location
+     */
+    public function actionUpdate($business_location_id, $store_uuid)
+    {
+        $model = $this->findModel ($business_location_id, $store_uuid);
 
+        $model->country_id = Yii::$app->request->getBodyParam ("country_id");
+        $model->business_location_name = Yii::$app->request->getBodyParam ("business_location_name");
+        $model->business_location_name_ar = Yii::$app->request->getBodyParam ("business_location_name_ar");
+        $model->support_pick_up = (int)Yii::$app->request->getBodyParam ("support_pick_up");
+        $model->business_location_tax = (double)Yii::$app->request->getBodyParam ("business_location_tax");
+        $model->mashkor_branch_id = Yii::$app->request->getBodyParam ("mashkor_branch_id");
+        $model->armada_api_key = Yii::$app->request->getBodyParam ("armada_api_key");
+        $model->address = Yii::$app->request->getBodyParam ("address");
+        $model->latitude = Yii::$app->request->getBodyParam ("latitude");
+        $model->longitude = Yii::$app->request->getBodyParam ("longitude");
 
-     /**
-      * Update Business Location
-      */
-     public function actionUpdate($business_location_id, $store_uuid)
-     {
+        if (!$model->save ()) {
+            if (isset($model->errors)) {
+                return [
+                    "operation" => "error",
+                    "message" => $model->errors
+                ];
+            } else {
+                return [
+                    "operation" => "error",
+                    "message" => "We've faced a problem updating the business location"
+                ];
+            }
+        }
 
-         $model = $this->findModel($business_location_id, $store_uuid);
-
-         $model->country_id =  Yii::$app->request->getBodyParam("country_id");
-         $model->business_location_name =  Yii::$app->request->getBodyParam("business_location_name");
-         $model->business_location_name_ar =  Yii::$app->request->getBodyParam("business_location_name_ar");
-         $model->support_pick_up = (int) Yii::$app->request->getBodyParam("support_pick_up");
-         $model->business_location_tax = (double) Yii::$app->request->getBodyParam("business_location_tax");
-         $model->mashkor_branch_id =  Yii::$app->request->getBodyParam("mashkor_branch_id");
-         $model->armada_api_key =  Yii::$app->request->getBodyParam("armada_api_key");
-         $model->address =  Yii::$app->request->getBodyParam("address");
-         $model->latitude =  Yii::$app->request->getBodyParam("latitude");
-         $model->longitude =  Yii::$app->request->getBodyParam("longitude");
-
-
-         if (!$model->save())
-         {
-             if (isset($model->errors)) {
-                 return [
-                     "operation" => "error",
-                     "message" => $model->errors
-                 ];
-             } else {
-                 return [
-                     "operation" => "error",
-                     "message" => "We've faced a problem updating the business location"
-                 ];
-             }
-         }
-
-         return [
-             "operation" => "success",
-             "message" => "Business Location updated successfully",
-             "model" => $model
-         ];
-     }
+        return [
+            "operation" => "success",
+            "message" => "Business Location updated successfully",
+            "model" => $model
+        ];
+    }
 
     /**
-    * Return Business Location detail
+     * Return Business Location detail
      * @param type $store_uuid
      * @param type $order_uuid
      * @return type
      */
-    public function actionDetail($store_uuid, $business_location_id) {
+    public function actionDetail($store_uuid, $business_location_id)
+    {
+        return $this->findModel ($business_location_id, $store_uuid);
+    }
 
-          $model = $this->findModel($business_location_id, $store_uuid);
-          return $model;
-  }
+    /**
+     * Delete Business Location
+     */
+    public function actionDelete($business_location_id, $store_uuid)
+    {
+        Yii::$app->accountManager->getManagedAccount ($store_uuid);
+        $model = $this->findModel ($business_location_id, $store_uuid);
 
+        if (!$model->delete ()) {
+            if (isset($model->errors)) {
+                return [
+                    "operation" => "error",
+                    "message" => $model->errors
+                ];
+            } else {
+                return [
+                    "operation" => "error",
+                    "message" => "We've faced a problem deleting the business location"
+                ];
+            }
+        }
 
-       /**
-        * Delete Business Location
-        */
-       public function actionDelete($business_location_id, $store_uuid)
-       {
-           Yii::$app->accountManager->getManagedAccount($store_uuid);
-           $model =  $this->findModel($business_location_id, $store_uuid);
+        return [
+            "operation" => "success",
+            "message" => "Business location deleted successfully"
+        ];
+    }
 
-           if (!$model->delete())
-           {
-               if (isset($model->errors)) {
-                   return [
-                       "operation" => "error",
-                       "message" => $model->errors
-                   ];
-               } else {
-                   return [
-                       "operation" => "error",
-                       "message" => "We've faced a problem deleting the business location"
-                   ];
-               }
-           }
+    /**
+     * Finds the Business Location model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return BusinessLocation the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($business_location_id, $store_uuid)
+    {
+        $store_model = Yii::$app->accountManager->getManagedAccount ($store_uuid);
 
-           return [
-               "operation" => "success",
-               "message" => "Business location deleted successfully"
-           ];
-       }
-
-
-
-      /**
-       * Finds the Business Location model based on its primary key value.
-       * If the model is not found, a 404 HTTP exception will be thrown.
-       * @param integer $id
-       * @return BusinessLocation the loaded model
-       * @throws NotFoundHttpException if the model cannot be found
-       */
-      protected function findModel($business_location_id, $store_uuid)
-      {
-          $store_model = Yii::$app->accountManager->getManagedAccount($store_uuid);
-
-          if (($model = BusinessLocation::find()->where(['business_location_id' => $business_location_id, 'restaurant_uuid' => $store_model->restaurant_uuid])->one()) !== null) {
-              return $model;
-          } else {
-              throw new NotFoundHttpException('The requested record does not exist.');
-          }
-      }
-
-
+        if (($model = BusinessLocation::find ()->where (['business_location_id' => $business_location_id, 'restaurant_uuid' => $store_model->restaurant_uuid])->one ()) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested record does not exist.');
+        }
+    }
 }
--- agent/modules/v1/controllers/ItemController.php
+++ agent/modules/v1/controllers/ItemController.php
@@ -9,17 +9,19 @@ use yii\helpers\Url;
 use yii\web\NotFoundHttpException;
 use agent\models\Item;
 
-class ItemController extends Controller {
+class ItemController extends Controller
+{
 
-    public function behaviors() {
-        $behaviors = parent::behaviors();
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors ();
 
         // remove authentication filter for cors to work
         unset($behaviors['authenticator']);
 
         // Allow XHR Requests from our different subdomains and dev machines
         $behaviors['corsFilter'] = [
-            'class' => \yii\filters\Cors::className(),
+            'class' => \yii\filters\Cors::className (),
             'cors' => [
                 'Origin' => Yii::$app->params['allowedOrigins'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
@@ -36,20 +38,21 @@ class ItemController extends Controller {
         ];
 
         // Bearer Auth checks for Authorize: Bearer <Token> header to login the user
-              $behaviors['authenticator'] = [
-                  'class' => \yii\filters\auth\HttpBearerAuth::className(),
-              ];
-              // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
-              $behaviors['authenticator']['except'] = ['options'];
+        $behaviors['authenticator'] = [
+            'class' => \yii\filters\auth\HttpBearerAuth::className (),
+        ];
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options'];
 
-              return $behaviors;
+        return $behaviors;
     }
 
     /**
      * @inheritdoc
      */
-    public function actions() {
-        $actions = parent::actions();
+    public function actions()
+    {
+        $actions = parent::actions ();
         $actions['options'] = [
             'class' => 'yii\rest\OptionsAction',
             // optional:
@@ -59,59 +62,172 @@ class ItemController extends Controller {
         return $actions;
     }
 
-
     /**
-    * Get all store's products
-     * @param type $store_uuid
+     * Get all store's products
      * @return type
      */
-     public function actionList($store_uuid) {
+    public function actionList()
+    {
+        $keyword = Yii::$app->request->get ('keyword');
 
-       $keyword = Yii::$app->request->get('keyword');
+        $store = Yii::$app->accountManager->getManagedAccount ();
 
-       Yii::$app->accountManager->getManagedAccount($store_uuid);
+        $query = $store->getItems();
 
+        if ($keyword) {
+            $query->filterWhere ([
+                    'or',
+                    ['like', 'item_name', $keyword],
+                    ['like', 'item_name_ar', $keyword],
+                    ['like', 'item_description', $keyword],
+                    ['like', 'item_description_ar', $keyword]
+                ]);
+        }
 
-       $query =  Item::find();
-
-       if ($keyword){
-         $query->where(['like', 'item_name', $keyword]);
-         $query->orWhere(['like', 'item_name_ar', $keyword]);
-         $query->orWhere(['like', 'item_description', $keyword]);
-         $query->orWhere(['like', 'item_description_ar', $keyword]);
-       }
-
-       $query->andWhere(['restaurant_uuid' => $store_uuid]);
-
-       return new ActiveDataProvider([
-           'query' => $query
-       ]);
-
-
-     }
-
-
+        return new ActiveDataProvider([
+            'query' => $query
+        ]);
+    }
 
     /**
-    * Return item detail
-     * @param type $store_uuid
-     * @param type $order_uuid
-     * @return type
+     * Return item detail
+     * @param string $id
+     * @return Item
      */
-    public function actionDetail($store_uuid, $item_uuid) {
+    public function actionDetail($id)
+    {
+        return $this->findModel($id);
+    }
 
-      if (Yii::$app->accountManager->getManagedAccount($store_uuid)) {
+    /**
+     * add new item
+     * @return array|string[]
+     * @throws NotFoundHttpException
+     */
+    public function actionCreate()
+    {
+        $store = Yii::$app->accountManager->getManagedAccount();
 
-          $item =  Item::find()
-                    ->where(['restaurant_uuid' => $store_uuid])
-                    ->andWhere(['item_uuid' => $item_uuid])
-                    ->one();
+        $model = new Item();
 
+        $model->restaurant_uuid = $store->restaurant_uuid;
+        $model->item_name = Yii::$app->request->getBodyParam ("name");
+        $model->item_name_ar = Yii::$app->request->getBodyParam ("name_ar");
+        $model->item_description = Yii::$app->request->getBodyParam ("description");
+        $model->item_description_ar = (int)Yii::$app->request->getBodyParam ("description_ar");
+        $model->sort_number = Yii::$app->request->getBodyParam ("sort_number");
+        $model->prep_time = Yii::$app->request->getBodyParam ("prep_time");
+        $model->prep_time_unit = Yii::$app->request->getBodyParam ("prep_time_unit");
+        $model->item_price = Yii::$app->request->getBodyParam ("price");
+        $model->sku = Yii::$app->request->getBodyParam ("sku");
+        $model->barcode = Yii::$app->request->getBodyParam ("barcode");
+        $model->track_quantity = Yii::$app->request->getBodyParam ("track_quantity");
+        $model->stock_qty = Yii::$app->request->getBodyParam ("track_quantity");
 
-          return $item;
+        if (!$model->save ()) {
+            if (isset($model->errors)) {
+                return [
+                    "operation" => "error",
+                    "message" => $model->errors
+                ];
+            } else {
+                return [
+                    "operation" => "error",
+                    "message" => "We've faced a problem creating the item"
+                ];
+            }
+        }
 
-      }
+        return [
+            "operation" => "success",
+            "message" => "Item created successfully"
+        ];
+    }
 
-  }
+    /**
+     * Update item
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel ($id);
 
+        $model->item_name = Yii::$app->request->getBodyParam ("name");
+        $model->item_name_ar = Yii::$app->request->getBodyParam ("name_ar");
+        $model->item_description = Yii::$app->request->getBodyParam ("description");
+        $model->item_description_ar = (int)Yii::$app->request->getBodyParam ("description_ar");
+        $model->sort_number = Yii::$app->request->getBodyParam ("sort_number");
+        $model->prep_time = Yii::$app->request->getBodyParam ("prep_time");
+        $model->prep_time_unit = Yii::$app->request->getBodyParam ("prep_time_unit");
+        $model->item_price = Yii::$app->request->getBodyParam ("price");
+        $model->sku = Yii::$app->request->getBodyParam ("sku");
+        $model->barcode = Yii::$app->request->getBodyParam ("barcode");
+        $model->track_quantity = Yii::$app->request->getBodyParam ("track_quantity");
+        $model->stock_qty = Yii::$app->request->getBodyParam ("track_quantity");
+
+        if (!$model->save ()) {
+            if (isset($model->errors)) {
+                return [
+                    "operation" => "error",
+                    "message" => $model->errors
+                ];
+            } else {
+                return [
+                    "operation" => "error",
+                    "message" => "We've faced a problem updating the item"
+                ];
+            }
+        }
+
+        return [
+            "operation" => "success",
+            "message" => "Item updated successfully"
+        ];
+    }
+
+    /**
+     * Delete Business Location
+     */
+    public function actionDelete($id)
+    {
+        $model = $this->findModel ($id);
+
+        if (!$model->delete ()) {
+            if (isset($model->errors)) {
+                return [
+                    "operation" => "error",
+                    "message" => $model->errors
+                ];
+            } else {
+                return [
+                    "operation" => "error",
+                    "message" => "We've faced a problem deleting the item"
+                ];
+            }
+        }
+
+        return [
+            "operation" => "success",
+            "message" => "Item deleted successfully"
+        ];
+    }
+
+    /**
+     * Finds the Item model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $item_uuid
+     * @return Item the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($item_uuid)
+    {
+        $store = Yii::$app->accountManager->getManagedAccount();
+
+        $model = $store->getItems()->findOne($item_uuid);
+
+        if ($model !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested record does not exist.');
+        }
+    }
 }
