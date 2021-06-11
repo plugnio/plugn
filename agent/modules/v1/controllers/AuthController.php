0dd08b8fc9a970c48c937eb423f3f8eb87375a61
Krushn Kathrecha <kathrechakrushn@gmail.com>
2021-06-11 14:14:13 +0530

    ability to update delivery area

--- agent/modules/v1/controllers/AuthController.php
+++ agent/modules/v1/controllers/AuthController.php
@@ -46,7 +46,7 @@ class AuthController extends Controller {
 
                 $agent = Agent::findByEmail($email);
 
-                if (true || $agent && $agent->validatePassword($password)) {
+                if ($agent && $agent->validatePassword($password)) {
                     return $agent;
                 }
 
