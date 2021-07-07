51975ef9ed9d75b0b78585ea481c378b02a0f423
saoud77 <saoudalturki@gmail.com>
2021-07-07 20:26:10 +0300

    trying to fix session issue

--- environments/staging/frontend/config/main-local.php
+++ environments/staging/frontend/config/main-local.php
@@ -19,7 +19,7 @@ $config = [
         'redis' => [
             'hostname' => 'plugn-redis.0x1cgp.0001.euw2.cache.amazonaws.com',
             'port' => 6379,
-            'database' => 4,
+            'database' => 9,
         ]
     ],
 ];
