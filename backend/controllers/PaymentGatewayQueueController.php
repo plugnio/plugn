c1da6a4f792abc7e3c57c97fd04a801fedcad5e2
saoud77 <saoudalturki@gmail.com>
2021-07-21 22:03:18 +0300

    hotfix

--- backend/controllers/PaymentGatewayQueueController.php
+++ backend/controllers/PaymentGatewayQueueController.php
@@ -15,21 +15,29 @@ use yii\filters\VerbFilter;
 class PaymentGatewayQueueController extends Controller
 {
   public $enableCsrfValidation = false;
-  
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
-    }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function behaviors() {
+      return [
+          'verbs' => [
+              'class' => VerbFilter::className(),
+              'actions' => [
+                  'delete' => ['POST'],
+              ],
+          ],
+          'access' => [
+              'class' => \yii\filters\AccessControl::className(),
+              'rules' => [
+                  [//allow authenticated users only
+                      'allow' => true,
+                      'roles' => ['@'],
+                  ],
+              ],
+          ],
+      ];
+  }
 
     /**
      * Lists all PaymentGatewayQueue models.
