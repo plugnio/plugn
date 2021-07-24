60b6e5974cc601893f15a8de789718c0a81b710d
saoud77 <saoudalturki@gmail.com>
2021-07-24 20:51:35 +0300

    hide sensutuve data

--- common/models/BusinessLocation.php
+++ common/models/BusinessLocation.php
@@ -84,6 +84,22 @@ class BusinessLocation extends \yii\db\ActiveRecord
     }
 
     /**
+     * @inheritdoc
+     */
+    public function fields()
+    {
+        $fields = parent::fields ();
+
+        // remove fields that contain sensitive information
+        unset($fields['mashkor_branch_id']);
+        unset($fields['armada_api_key']);
+
+        return $fields;
+
+    }
+    
+
+    /**
      * Gets query for [[Country]].
      *
      * @return \yii\db\ActiveQuery
