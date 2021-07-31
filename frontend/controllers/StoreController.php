f480fd574aabd8558f0198c098ddccf690ace8d6
saoud77 <saoudalturki@gmail.com>
2021-07-31 16:15:48 +0300

    fix stat issye

--- frontend/controllers/StoreController.php
+++ frontend/controllers/StoreController.php
@@ -82,7 +82,7 @@ class StoreController extends Controller {
 
         array_push($revenue_generated_chart_data, $revenue_generated_last_five_months_month ? (double) $revenue_generated_last_five_months_month : 0);
 
-        array_push($months, $lastFiveMonths);
+        // array_push($months, $lastFiveMonths);
 
 
 
@@ -94,7 +94,7 @@ class StoreController extends Controller {
 
         array_push($revenue_generated_chart_data, $revenue_generated_last_four_months_month ? (double) $revenue_generated_last_four_months_month : 0);
 
-        array_push($months, $lastFoureMonths);
+        // array_push($months, $lastFoureMonths);
 
 
         $revenue_generated_last_three_months_month = Order::find()
@@ -106,7 +106,7 @@ class StoreController extends Controller {
 
         array_push($revenue_generated_chart_data, $revenue_generated_last_three_months_month ? (double) $revenue_generated_last_three_months_month : 0);
 
-        array_push($months, $lastThreeMonths);
+        // array_push($months, $lastThreeMonths);
 
 
         $revenue_generated_last_two_months_month = Order::find()
@@ -117,7 +117,7 @@ class StoreController extends Controller {
 
         array_push($revenue_generated_chart_data, $revenue_generated_last_two_months_month ? (double) $revenue_generated_last_two_months_month : 0 );
 
-        array_push($months, $lastTwoMonths);
+        // array_push($months, $lastTwoMonths);
 
 
         $revenue_generated_last_month = Order::find()
@@ -126,7 +126,7 @@ class StoreController extends Controller {
 
         $lastMonth = date('M', strtotime('-1 months'));
         array_push($revenue_generated_chart_data, $revenue_generated_last_month ? (double) $revenue_generated_last_month : 0);
-        array_push($months, $lastMonth);
+        // array_push($months, $lastMonth);
 
 
         $revenue_generated_current_month = Order::find()
@@ -136,7 +136,7 @@ class StoreController extends Controller {
 
         array_push($revenue_generated_chart_data, $revenue_generated_current_month ? (double) $revenue_generated_current_month : 0);
 
-        array_push($months, $currentMonth);
+        // array_push($months, $currentMonth);
 
 
         $order_recevied_chart_data = [];
@@ -255,6 +255,10 @@ class StoreController extends Controller {
             array_push($number_of_sold_items_chart_data, $item->unit_sold ? $item->unit_sold : 0);
         }
 
+
+            for ($i = 5; $i >= 0; $i--) {
+                 $months[] = date("F", strtotime( date( 'Y-m-01' )." -$i months"));
+          }
 
 
         return $this->render('statistics', [
