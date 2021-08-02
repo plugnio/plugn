f9dcde7decfbd6e544daadfb0eaccef886f98703
Krushn Kathrecha <kathrechakrushn@gmail.com>
2021-08-02 21:33:37 +0530

    revert

--- agent/modules/v1/controllers/AuthController.php
+++ agent/modules/v1/controllers/AuthController.php
@@ -49,7 +49,7 @@ class AuthController extends Controller {
 
                 $agent = Agent::findByEmail($email);
 
-                if (true || $agent && $agent->validatePassword($password)) {
+                if ($agent && $agent->validatePassword($password)) {
                     return $agent;
                 }
 
