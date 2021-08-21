0c363ed7a02a353c8c873862d8fc955d68cd5d26
saoud77 <saoudalturki@gmail.com>
2021-08-21 19:07:20 +0300

    Order detail page form: Page is not working

--- environments/index.php
+++ environments/index.php
@@ -62,6 +62,37 @@ return [
             'shortner/config/main-local.php',
         ],
     ],
+    'Development [Saoud]' => [
+        'path' => 'dev-saoud',
+        'setWritable' => [
+            'backend/runtime',
+            'backend/web/assets',
+            'backend/web/uploads/project-files',
+            'console/runtime',
+            'frontend/runtime',
+            'frontend/web/assets',
+            'frontend/web/uploads/',
+            'api/runtime',
+            'api/web/assets',
+            'agent/runtime',
+            'agent/web/assets',
+            'shortner/runtime'
+        ],
+        'setExecutable' => [
+            'yii',
+            'yii_test',
+            'fixture-generate-and-load',
+            'run-tests.sh'
+        ],
+        'setCookieValidationKey' => [
+            'backend/config/main-local.php',
+            'common/config/codeception-local.php',
+            'frontend/config/main-local.php',
+            'api/config/main-local.php',
+            'agent/config/main-local.php',
+            'shortner/config/main-local.php',
+        ],
+    ],
     'Development' => [
         'path' => 'dev',
         'setWritable' => [
--- frontend/views/order-item/create.php
+++ frontend/views/order-item/create.php
@@ -4,11 +4,11 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 
-$this->params['restaurant_uuid'] = $model->restaurant->restaurant_uuid;
+$this->params['restaurant_uuid'] =  $store->restaurant_uuid;
 
 $this->title = 'Create Order Item';
-$this->params['breadcrumbs'][] = ['label' => 'Order #' . $model->order_uuid, 'url' => ['order/view','id' => $model->order_uuid, 'storeUuid' =>$model->restaurant->restaurant_uuid ]];
-$this->params['breadcrumbs'][] = ['label' => 'Update', 'url' => ['order/update','id' => $model->order_uuid, 'storeUuid' =>$model->restaurant->restaurant_uuid ]];
+$this->params['breadcrumbs'][] = ['label' => 'Order #' . $model->order_uuid, 'url' => ['order/view','id' => $model->order_uuid, 'storeUuid' => $store->restaurant_uuid ]];
+$this->params['breadcrumbs'][] = ['label' => 'Update', 'url' => ['order/update','id' => $model->order_uuid, 'storeUuid' => $store->restaurant_uuid ]];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="order-item-create">
