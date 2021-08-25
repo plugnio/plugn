d28ed17b3d4eff3dde2e4410a52f4b36f78a7083
saoud77 <saoudalturki@gmail.com>
2021-08-25 14:22:26 +0300

    allowed all ips on debug

--- environments/staging/partner/config/main-local.php
+++ environments/staging/partner/config/main-local.php
@@ -25,12 +25,12 @@ return [
 ];
 
 
-if (!YII_ENV_TEST) {
+if (YII_DEBUG) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        'allowedIPs' => ['*']
+        'allowedIPs' => ['*'],
     ];
 
     $config['bootstrap'][] = 'gii';
