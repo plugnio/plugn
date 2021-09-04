0a31a0dc1cbff0852efbfc2524a0cef04c9931a3
saoud77 <saoudalturki@gmail.com>
2021-09-05 00:22:35 +0300

    hotFix2

--- frontend/views/order/invoice.php
+++ frontend/views/order/invoice.php
@@ -381,7 +381,8 @@ $this->params['restaurant_uuid'] = $model->restaurant_uuid;
                             [
                                 'label' => 'Subtotal',
                                 'value' => function ($orderItem) {
-                                    return Yii::$app->formatter->asCurrency($orderItem->item_price,'KWD');
+                                    return Yii::$app->formatter->asCurrency($orderItem->item_price,
+                                        $orderItem->currency->code);
                                 }
                             ],
                         ],
