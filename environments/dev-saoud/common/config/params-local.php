ec16bf4d85d570ee7194ad8b269d1bc2c4c462fb
Krushn Kathrecha <kathrechakrushn@gmail.com>
2021-09-24 18:08:22 +0530

    onesignal keys updated

--- environments/dev-saoud/common/config/params-local.php
+++ environments/dev-saoud/common/config/params-local.php
@@ -2,4 +2,8 @@
 return [
   'apiEndpoint' => 'http://localhost/~Saoud/plugn/plugn-yii2/api/web',
   'frontendUrl' => 'http://localhost/~Saoud/plugn/plugn-yii2/frontend/web',
+  'oneSignalStoreAPPID' => '',
+    'oneSignalStoreAPIKey' => '',
+    'oneSignalAgentAPPID' => '83eb72f3-98c8-48a7-8b40-57736914f1cb',
+    'oneSignalAgentAPIKey' => 'MmVhMTNhZTUtMmY1Zi00Y2YyLWEzNWYtZjYyMWIxNTk1YzUx'
 ];
