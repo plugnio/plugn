b93eacfba0f18a565937297647758198deeb4064
saoud77 <saoudalturki@gmail.com>
2022-03-08 18:52:57 +0300

    Update env

--- environments/dev-server/common/config/main-local.php
+++ environments/dev-server/common/config/main-local.php
@@ -43,13 +43,30 @@ return [
                 ],
             ],
         ],
+        // 'mailer' => [
+        //          'class' => 'yii\swiftmailer\Mailer',
+        //          'viewPath' => '@common/mail',
+        //          // send all mails to a file by default. You have to set
+        //          // 'useFileTransport' to false and configure a transport
+        //          // for the mailer to send real emails.
+        //          'useFileTransport' => true,
+        // ],
         'mailer' => [
-                 'class' => 'yii\swiftmailer\Mailer',
-                 'viewPath' => '@common/mail',
-                 // send all mails to a file by default. You have to set
-                 // 'useFileTransport' to false and configure a transport
-                 // for the mailer to send real emails.
-                 'useFileTransport' => true,
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.sendgrid.net',
+                'username' => 'apikey',
+                'password' => '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',
+                'port' => '587',
+                'encryption' => 'tls',
+                // 'plugins' => [
+                //     [
+                //         'class' => 'Openbuildings\Swiftmailer\CssInlinerPlugin',
+                //     ],
+                // ],
+            ],
         ],
         'cache' => [
             // Use Redis as a cache
