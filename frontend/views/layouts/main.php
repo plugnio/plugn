42f8a5a9ea7d8d95f95ab2f9d02dddb783ae7ffe
saoud77 <saoudalturki@gmail.com>
2022-04-05 20:35:50 +0300

    Add hotjar

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -33,7 +33,22 @@ $restaurant_model = Restaurant::find()->where(['restaurant_uuid' => $this->param
         </title>
         <link rel="shortcut icon" href="<?php echo Yii::$app->request->baseUrl; ?>/favicon.ico" type="image/x-icon" />
 
+
         <?php $this->head() ?>
+
+        <!-- Hotjar Tracking Code for Plugn - Old dashboard -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2120272,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+
     </head>
 
     <!-- END: Head-->
