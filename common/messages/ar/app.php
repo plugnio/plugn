840babd8ecde9d560761c4e158ec4d594d28458b
krushn <kathrechakrushn@gmail.com>
2022-04-11 13:43:52 +0530

    arabic

--- common/messages/ar/app.php
+++ common/messages/ar/app.php
@@ -17,6 +17,8 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
+    'Page Title' => "عنوان الصفحة",
+    'Page Title in Arabic' => "عنوان الصفحة باللغة العربية",
     'Option Type' => "نوع الخيار",
     "Is Required?" => "مطلوب؟",
     'Incorrect email or password.' => 'بريد أو كلمة مرورغير صحيحة.',
