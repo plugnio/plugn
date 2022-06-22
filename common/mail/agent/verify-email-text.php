274d9c11da093563229a591c9799658df4339a68
Krushn Kathrecha <kathrechakrushn@gmail.com>
2022-06-22 18:36:42 +0530

    hotfix app url

--- common/mail/agent/verify-email-text.php
+++ common/mail/agent/verify-email-text.php
@@ -5,7 +5,7 @@
 
 //$verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'token' => $agent->agent_auth_key]);
 
-$verifyLink = Yii::$app->params['dashboardAppUrl'] . '/verify-email/' . urlencode($email) . '/' . $agent->agent_auth_key;
+$verifyLink = Yii::$app->params['newDashboardAppUrl'] . '/verify-email/' . urlencode($email) . '/' . $agent->agent_auth_key;
 
 ?>
 Hello <?= $agent->agent_name ?>,
--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -1,13 +1,14 @@
 <?php
+
 return [
   'apiEndpoint' => 'http://localhost/~Saoud/plugn/plugn-yii2/api/web',
   'frontendUrl' => 'http://localhost/~Saoud/plugn/plugn-yii2/frontend/web',
   'dashboardAppUrl' => 'https://dash.staging.plugn.io',
   'dashboardCookieDomain' => 'dash.dev.plugn.io',
-    'newDashboardAppUrl' => 'https://dash.dev.plugn.io',
+  'newDashboardAppUrl' => 'https://dash.dev.plugn.io',
   'oneSignalStoreAPPID' => '',
-    'oneSignalStoreAPIKey' => '',
-    'oneSignalAgentAPPID' => '83eb72f3-98c8-48a7-8b40-57736914f1cb',
-    'oneSignalAgentAPIKey' => 'MmVhMTNhZTUtMmY1Zi00Y2YyLWEzNWYtZjYyMWIxNTk1YzUx',
-    'currencylayer_api_key' => 'b9d4093e0c8327cbc624b9239450f72d'
+  'oneSignalStoreAPIKey' => '',
+  'oneSignalAgentAPPID' => '83eb72f3-98c8-48a7-8b40-57736914f1cb',
+  'oneSignalAgentAPIKey' => 'MmVhMTNhZTUtMmY1Zi00Y2YyLWEzNWYtZjYyMWIxNTk1YzUx',
+  'currencylayer_api_key' => 'b9d4093e0c8327cbc624b9239450f72d'
 ];
