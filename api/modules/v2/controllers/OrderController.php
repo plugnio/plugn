28fe666523def6fb18eecea32050d57eb098af47
Krushn Kathrecha <kathrechakrushn@gmail.com>
2022-09-21 18:40:22 +0530

    space

--- api/modules/v2/controllers/OrderController.php
+++ api/modules/v2/controllers/OrderController.php
@@ -341,6 +341,7 @@ class OrderController extends Controller
                     try {
 
                         // Validate that theres no error from TAP gateway
+
                         if (isset($responseContent->status) && $responseContent->status == "fail") {
                             $transaction->rollBack();
 
