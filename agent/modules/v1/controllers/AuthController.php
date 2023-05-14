b26f0a6427a087d55b4a54199d758e00000a41f7
Krushn Kathrecha <kathrechakrushn@gmail.com>
2023-05-14 14:49:54 +0530

    zapier store creation event

--- agent/modules/v1/controllers/AuthController.php
+++ agent/modules/v1/controllers/AuthController.php
@@ -402,6 +402,16 @@ class AuthController extends Controller {
                 
                 Yii::$app->auth0->createUser($param);
 
+                //https://hooks.zapier.com/hooks/catch/3784096/oeap6qy
+
+                Yii::$app->zapier->webhook("https://hooks.zapier.com/hooks/catch/3784096/366cqik", [
+                    'first_name' => trim($firstname),
+                    'last_name' => trim($lastname),
+                    'store_name' => $store->name,
+                    'phone_number' => $store->owner_phone_country_code . $store->owner_number,
+                    'email' => $agent->agent_email,
+                    'store_url' => $store->restaurant_domain
+                ]);
             }
 
             $transaction->commit();
