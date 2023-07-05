93efe1472d6f968f7e1350b6e282712b67d4beed
Er. Krushn Kathrecha <kathrechakrushn@gmail.com>
2023-07-05 14:42:08 +0530

    hotfix

--- agent/modules/v1/controllers/StoreController.php
+++ agent/modules/v1/controllers/StoreController.php
@@ -187,13 +187,13 @@ class StoreController extends BaseController
         $store->owner_number = Yii::$app->request->getBodyParam('owner_number');
         $store->owner_phone_country_code = Yii::$app->request->getBodyParam('owner_phone_country_code');
 
-        if(strpos($store->restaurant_domain, ".plugn.store") == -1) {
+        /*if(strpos($store->restaurant_domain, ".plugn.store") == -1) {
             $store->restaurant_domain = $store->restaurant_domain . ".plugn.store";
         }
 
         if(strpos($store->restaurant_domain, "http") == -1) {
             $store->restaurant_domain = "https://" . $store->restaurant_domain;
-        }
+        }*/
 
         if(!$store->restaurant_email) {
             $store->restaurant_email = Yii::$app->user->identity->agent_email;
