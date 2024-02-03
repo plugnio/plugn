3bb29b63df1b3c82ea607a7fbc6f668dee551366
Er. Krushn Kathrecha <kathrechakrushn@gmail.com>
2024-02-03 11:24:05 +0530

    backup mail server

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -105,18 +105,20 @@ return [
             'viewPath' => '@common/mail',
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
-                'host' => 'email-smtp.eu-west-1.amazonaws.com',
-                'username' => 'AKIAWMITDJRKVNB2AFUL',//AKIAWMITDJRKTH5HBB2O //AKIAWMITDJRKTQGXUQT3
-                'password' => 'BFXl6illZPE3NP5EQrVNbCO+gMBCopuIi/uy5nwCsUZ6',//BKyPcINpZJsEVnUrMGymff27eaIztgNwSWN7xI2960eJ //GDkiUbOkIxx4qpd0fcksh//0qKvAITbj4PCywBjh
-                'port' => '587',
-                'encryption' => 'tls',
-                /*
-                'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.elasticemail.com',
                 'username' => 'support@plugn.io',
                 'password' => 'CD006D3ED0C5BD796D1D2C77B111CC24AF9E',
                 'port' => '2525',
-                'encryption' => 'tls'*/
+                'encryption' => 'tls'
+                /*
+               'class' => 'Swift_SmtpTransport',
+               'host' => 'email-smtp.eu-west-1.amazonaws.com',
+               'username' => 'AKIAWMITDJRKVNB2AFUL',//AKIAWMITDJRKTH5HBB2O //AKIAWMITDJRKTQGXUQT3
+               'password' => 'BFXl6illZPE3NP5EQrVNbCO+gMBCopuIi/uy5nwCsUZ6',//BKyPcINpZJsEVnUrMGymff27eaIztgNwSWN7xI2960eJ //GDkiUbOkIxx4qpd0fcksh//0qKvAITbj4PCywBjh
+               'port' => '587',
+               'encryption' => 'tls',
+
+               */
 
                 /*
                 'class' => 'Swift_SmtpTransport',
