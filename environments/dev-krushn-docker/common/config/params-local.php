94949db52ac3aa9345c9c09757ef4fabcda1481f
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2025-02-09 19:01:53 +0530

    part of tech-992 : plugn dockerisation

---
+++ environments/dev-krushn-docker/common/config/params-local.php
@@ -0,0 +1,14 @@
+<?php
+return [
+  'apiEndpoint' => 'http://localhost/plugn/api/web',
+  'frontendUrl' => 'http://localhost/plugn/frontend/web',
+  'dashboardAppUrl' => 'http://localhost:8100',
+    "crmAppUrl"  => 'http://localhost:8100',
+  'dashboardCookieDomain' => 'localhost',
+    'newDashboardAppUrl' => 'https://dash.dev.plugn.io',
+    'oneSignalStoreAPPID' => '',
+    'oneSignalStoreAPIKey' => '',
+    'oneSignalAgentAPPID' => '81208dad-babc-4c82-98eb-b67fcbdee8fd',//83eb72f3-98c8-48a7-8b40-57736914f1cb
+    'oneSignalAgentAPIKey' => 'N2M5NTY4NTUtNWI1Yy00Y2M4LTg0NGMtMjZiNGZjNmU2Mzky',//MmVhMTNhZTUtMmY1Zi00Y2YyLWEzNWYtZjYyMWIxNTk1YzUx
+    'currencylayer_api_key' => 'b9d4093e0c8327cbc624b9239450f72d'
+];
