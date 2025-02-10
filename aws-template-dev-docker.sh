162dc22c17d781540a849947afe6348da20b4b9b
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2025-02-10 21:12:56 +0530

    part of tech-994 : missing packages + aws template

---
+++ aws-template-dev-docker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Update the package repository and install required packages
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y docker.io unzip curl gh docker-compose
+
+# Start and enable Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Add the 'ubuntu' user to the Docker group
+sudo usermod -aG docker ubuntu
+
+# gir repo setup 1st way
+apt install -y openssh-clients
+ps -auxc | grep ssh-agent
+eval $(ssh-agent)
+sudo mkdir -p /var/www/html
+sudo chmod 2775 /var/www
+cd /var/www/html
+
+echo "github private key" > ~/.ssh/github
+chmod go-rw ~/.ssh/github
+echo "github public key" > ~/.ssh/github.pub
+#or ssh-keygen -y -f ~/.ssh/github > ~/.ssh/github.pub
+#sudo chmod a+r ~/.ssh/github
+ssh-add ~/.ssh/github
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+apt install -y git
+git clone git@github.com:plugnio/plugn.git /var/www/html
+#cd ./plugn
+cd /var/www/html
+git remote add git@github.com:plugnio/plugn.git
+git checkout develop
+git config --global --add safe.directory /var/www/html
+
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+
+docker-compose -f docker-compose-dev.yml -p plugn-dev-server up -d
---
+++ aws-template-docker.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Update the package repository and install required packages
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y docker.io unzip curl gh docker-compose
+
+# Start and enable Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Add the 'ubuntu' user to the Docker group
+sudo usermod -aG docker ubuntu
+
+# gir repo setup 1st way
+apt install -y openssh-clients
+ps -auxc | grep ssh-agent
+eval $(ssh-agent)
+sudo mkdir -p /var/www/html
+sudo chmod 2775 /var/www
+cd /var/www/html
+
+echo "github private key" > ~/.ssh/github
+chmod go-rw ~/.ssh/github
+echo "github public key" > ~/.ssh/github.pub
+#or ssh-keygen -y -f ~/.ssh/github > ~/.ssh/github.pub
+#sudo chmod a+r ~/.ssh/github
+ssh-add ~/.ssh/github
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+apt install -y git
+git clone git@github.com:plugnio/plugn.git /var/www/html
+#cd ./plugn
+cd /var/www/html
+git remote add git@github.com:plugnio/plugn.git
+git checkout main
+git config --global --add safe.directory /var/www/html
+
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+
+docker-compose -f docker-compose-prod.yml -p plugn-prod-server up -d
+
+# Install AWS CLI
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#sudo apt-get install -y unzip # Install unzip if not already available
+#unzip awscliv2.zip
+#sudo ./aws/install
+#rm -rf awscliv2.zip aws
+
+# Authenticate Docker to Amazon ECR
+#aws ecr get-login-password --region eu-west-2 | sudo docker login --username AWS --password-stdin 438663597141.dkr.ecr.eu-west-2.amazonaws.com
+
+# Pull the Docker image from ECR
+#sudo docker pull 438663597141.dkr.ecr.eu-west-2.amazonaws.com/plugn/backend-prod
+
+# Run the Docker container
+#sudo docker run -d -p 80:80 --name plugn-backend-prod 438663597141.dkr.ecr.eu-west-2.amazonaws.com/plugn/backend-prod
